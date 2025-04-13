6c03a859a6c6a1ae219587be169a3bb42398e326
cuong <cuongngn.tech@gmail.com>
2025-04-13 09:08:04 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.static(path.join(__dirname, "public")));
+
+// Routes
+const indexRoute = require("./routes/indexRoute");
+app.use("/", indexRoute);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(
+    `🚀 Server đang chạy tại: \x1b[34mhttp://localhost:${PORT}\x1b[0m`
+  );
+});
