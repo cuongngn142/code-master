0452dd2ae700140f79c6eb8e28e01c60faa00907
cuong <cuongngn.tech@gmail.com>
2025-04-20 23:01:37 +0700

    change some frontend design

--- config/database.js
+++ config/database.js
@@ -1,24 +1,25 @@
-const sql = require('mssql');
+const sql = require("mssql");
 
 const config = {
-    server: 'LAPTOP-THEVU210\\THEVU',
-    user: 'thevu2109',
-    password: 'thevu2109',
-    database: 'codemasterDb', // Thay đổi từ 'master' thành 'codemasterDb'
-    options: {
-        trustServerCertificate: true,
-        encrypt: false
-    }
+  server: "serverName",
+  user: "taiKhoan",
+  password: "matKhau",
+  database: "codemasterDb", // Thay đổi từ 'master' thành 'codemasterDb'
+  options: {
+    trustServerCertificate: true,
+    encrypt: false,
+  },
 };
 
 const poolPromise = new sql.ConnectionPool(config)
-    .connect()
-    .then(pool => {
-        console.log('Connected to SQL Server');
-        return pool;
-    })
-    .catch(err => console.log('Database Connection Failed! Bad Config: ', err));
+  .connect()
+  .then((pool) => {
+    console.log("Connected to SQL Server");
+    return pool;
+  })
+  .catch((err) => console.log("Database Connection Failed! Bad Config: ", err));
 
 module.exports = {
-    sql, poolPromise
+  sql,
+  poolPromise,
 };
